5eedd9761006f2487912cb40fcd80c4c702769b5
Hritikraj8804 <hritikraj8804@gmail.com>
2025-08-14 23:51:03 +0530

    simple chain

---
+++ Chains/simple_chain.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='genernate 5 name from {country}',
+    input_variables=['country']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'country': 'korea'})
+
+print(result)
+
+chain.get_graph().print_ascii()
